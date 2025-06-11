d2bc5fae0623e7e775eab673be4796b03322a66d
unknown <riddhimaroy10@gmail.com>
2025-06-11 18:42:56 +0530

    static pages finished

--- project2/script.js
+++ project2/script.js
@@ -4,3 +4,27 @@ $(document).on('click', '.lms_show', function(){
     $('.accn').toggleClass('active');
     $('.lms_show').toggleClass('active');
 })
+
+$(document).on('mouseenter', '.servbtn', function() {
+    $('.menudrop').show();
+})
+
+$(document).on('mouseleave', '.servbtn', function() {
+    $('.menudrop').hide();
+})
+
+$(document).on('mouseenter', '.menudrop', function() {
+    $('.menudrop').show();
+})
+
+$(document).on('mouseleave', '.menudrop', function() {
+    $('.menudrop').hide();
+})
+
+$(document).on('mouseenter', '.md', function() {
+    $(this).css("background-color", "#008080");
+})
+
+$(document).on('mouseleave', '.md', function() {
+    $(this).css("background-color", "#323956");
+})
